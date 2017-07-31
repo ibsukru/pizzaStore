c7a8589a42516faad48cbfdc55e9d2d2c422c4ca
isukru <isukru@travix.com>
2017-07-31 11:42:04 +0200

    Ecapsulation for pizza component

--- src/components/pizza/index.js
+++ src/components/pizza/index.js
@@ -1,3 +1,13 @@
 import Pizza from './Pizza'
+import { connect } from 'react-redux'
+import { selectSize } from '../../actions'
 
-export default Pizza
+const mapDispatchToProps = dispatch => {
+  return {
+    onSelectingSize: size => {
+      dispatch(selectSize(size))
+    }
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Pizza)
